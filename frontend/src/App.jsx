724f4ad3a8a9b459798f1ca1df66e860046c3937
Mamunur1093 <mamunur1093@gmail.com>
2025-10-25 21:17:43 +0600

    Installed React frontend using Tailwind CSS

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-center">Hello New World!</h1>
+      <h2 className='text-center text-amber-700'>Start editing to see some magic happen</h2>
+    </>
+  )
+}
+
+export default App
